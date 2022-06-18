976c9a36beb0f4990566114c28f6bb56a9876974
zm102905 <zm102905@gmail.com>
2022-06-18 13:06:42 -0400

    wrote Jave for Habitat model, repository, and Controller. Created controller method that displays single habitat for habitat page

--- src/main/java/org/wecancodeit/birdwatcher/HabitatController.java
+++ src/main/java/org/wecancodeit/birdwatcher/HabitatController.java
@@ -1,2 +1,22 @@
+package org.wecancodeit.birdwatcher;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+
+@Controller
 public class HabitatController {
+
+    @Resource
+    private HabitatRepository habitatRepo;
+
+    @RequestMapping("/habitat/{name}")
+    public String displaySingleHabitat(@PathVariable String name, Model model){
+       Habitat retrievedHabitat = habitatRepo.findHabitatByName(name);
+       model.addAttribute("habitat", retrievedHabitat);
+       return "habitatTemplate";
+    }
 }
