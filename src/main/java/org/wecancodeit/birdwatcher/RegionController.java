4e874161ebd88dd9fefa1f5c24e270fc1b38b485
samleivadev <samdleiva@gmail.com>
2022-06-19 13:36:01 -0400

    contorller for individual region

--- src/main/java/org/wecancodeit/birdwatcher/RegionController.java
+++ src/main/java/org/wecancodeit/birdwatcher/RegionController.java
@@ -5,17 +5,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.Resource;
+
 @Controller
 public class RegionController {
+
+    @Resource
+    private RegionRepository regionRepo;
+
     @RequestMapping({"/Region", "/", ""})
     public String displayRegions(Model model) {
-        model.addAttribute("regions", regionStorage.findAllRegions());
+        model.addAttribute("regions", regionRepo.findAll());
         return "regionsView";
     }
 
     @GetMapping("/regions/{regionLocation}")
-    public String displaySingleCampus(@PathVariable String campusLocation, Model model) {
-        Region retrievedRegion = regionStorage.findCampusByLocation(campusLocation);
+    public String displaySingleRegion(@PathVariable String regionName, Model model) {
+        Region retrievedRegion = regionRepo.findByName(regionName);
         model.addAttribute("region", retrievedRegion);
 
         return "regionView";
