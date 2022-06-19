13da78cbf1ae703e5c898ad9d679ad1aca191cb0
samleivadev <samdleiva@gmail.com>
2022-06-19 13:17:38 -0400

    Region Controller in progress

--- src/main/java/org/wecancodeit/birdwatcher/RegionController.java
+++ src/main/java/org/wecancodeit/birdwatcher/RegionController.java
@@ -1,6 +1,23 @@
 package org.wecancodeit.birdwatcher;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RegionController {
+    @RequestMapping({"/Region", "/", ""})
+    public String displayRegions(Model model) {
+        model.addAttribute("regions", regionStorage.findAllRegions());
+        return "regionsView";
+    }
+
+    @GetMapping("/regions/{regionLocation}")
+    public String displaySingleCampus(@PathVariable String campusLocation, Model model) {
+        Region retrievedRegion = regionStorage.findCampusByLocation(campusLocation);
+        model.addAttribute("region", retrievedRegion);
+
+        return "regionView";
+    }
 }
